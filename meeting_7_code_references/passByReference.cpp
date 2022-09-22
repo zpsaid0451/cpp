805ff206093143b0d523bbab46cfa95e34ef23ca
Ping <zpsaid0451@gmail.com>
2022-09-22 13:35:31 -0400

    project 1 open files

--- meeting_7_code_references/passByReference.cpp
+++ meeting_7_code_references/passByReference.cpp
@@ -24,3 +24,24 @@ int addOne(int &a){
   cout << "During addOne call myInt = " << a << endl;
   return (++a); //What happens if we use a++ instead of ++a?
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// void addOne(int &a);
+
+// int main () {
+//   int myInt = 10;
+//   cout << "Before calling addOne myInt = " << myInt << endl;
+//   addOne(myInt); //Notice not storing result
+//   cout << "After calling addOne myInt = " << myInt << endl;
+//   myInt = addOne(myInt); //Notice storing result as myInt
+//   cout << "After returning results of function call as myInt = " << myInt << endl;
+//   return 0;
+// }
+
+// int addOne(int &a){
+//   cout << "During addOne call myInt = " << a << endl;
+//   ++a; //What happens if we use a++ instead of ++a?
+// }
--- proj1/proj1.cpp
+++ proj1/proj1.cpp
@@ -10,22 +10,34 @@ Description: This is project one
 using namespace std;
 
 int main () {
-    string fName; //Declaration of a string
-    string lName; //Declaration of a string
-    int age;
-    ifstream myfile ("example.txt"); //Declares and opens a file
+    // string fName; //Declaration of a string
+    // string lName; //Declaration of a string
+    // int age;
+    string hashtagFile;
+    string parseFile;
+
+    // ifstream myfile ("example.txt"); //Declares and opens a file
+    cout << "Welcome to the Hashtag Analyzer \nYou will choose a file of hashtags to search for \nThen you will choose a file to search for the hashtags" << endl;
+    cout << "Please enter the file with the hashtags you would like to use:" << endl; 
+	cin >> hashtagFile; //console input
+    cout << "Please enter the file with the messages you would like to parse:" << endl; 
+	cin >> parseFile; //console input
+    ifstream myHashtagFile (hashtagFile); 
+    ifstream myParseFile (parseFile); 
+    if (myHashtagFile.is_open()) {
+        cout << "Your file was imported!\n" << endl;
+        
+         //Checks to see if the file exists and was opened
+    //   while(myfile >> fName){ //Keeps reading a name as long as there is another line
+    //     myfile >> lName >> age; //Reads the rest of the line
+    //     cout << "Your name is: " << fName << " " << lName << endl; //Outputs the first and last names
+    //     cout << "You are: " << age << " years old." << endl; //Outputs the age
+    //   }
     
-    cout << "Please enter the file with the hashtags you would like to use:" << endl; //user prompt
-	cin >> file; //console input
-    if (myfile.is_open()) { //Checks to see if the file exists and was opened
-      while(myfile >> fName){ //Keeps reading a name as long as there is another line
-        myfile >> lName >> age; //Reads the rest of the line
-        cout << "Your name is: " << fName << " " << lName << endl; //Outputs the first and last names
-        cout << "You are: " << age << " years old." << endl; //Outputs the age
-      }
     }else {
       cout << "Unable to open file"; //If the input file does not exist, notifies user
     }
-      myfile.close(); //Closes the file
+      myHashtagFile.close(); //Closes the file
   return 0;
 }
+
