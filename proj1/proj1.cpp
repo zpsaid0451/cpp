d1a870837dbd34fa23e6baf3ee30f244928b1e05
Ping <zpsaid0451@gmail.com>
2022-09-25 21:47:15 -0400

    added while loop for file not exist

--- proj1/proj1.cpp
+++ proj1/proj1.cpp
@@ -15,6 +15,13 @@ int main () {
     // int age;
     string hashtagFile;
     string parseFile;
+    ifstream myHashtagFile;
+    ifstream myParseFile;
+    string hashtag;
+    string parse;
+    string hashTag;
+    int hashtagSize = 0;
+    int parseSize =0;
 
     // ifstream myfile ("example.txt"); //Declares and opens a file
     cout << "Welcome to the Hashtag Analyzer \nYou will choose a file of hashtags to search for \nThen you will choose a file to search for the hashtags" << endl;
@@ -22,22 +29,63 @@ int main () {
 	cin >> hashtagFile; //console input
     cout << "Please enter the file with the messages you would like to parse:" << endl; 
 	cin >> parseFile; //console input
-    ifstream myHashtagFile (hashtagFile); 
-    ifstream myParseFile (parseFile); 
-    if (myHashtagFile.is_open()) {
-        cout << "Your file was imported!\n" << endl;
-        
-         //Checks to see if the file exists and was opened
-    //   while(myfile >> fName){ //Keeps reading a name as long as there is another line
-    //     myfile >> lName >> age; //Reads the rest of the line
-    //     cout << "Your name is: " << fName << " " << lName << endl; //Outputs the first and last names
-    //     cout << "You are: " << age << " years old." << endl; //Outputs the age
-    //   }
+    // ifstream myHashtagFile (hashtagFile); 
+    // ifstream myParseFile (parseFile); 
+
+    // handleHashtagFile (myHashtagFile);
+    // handleParseFile(myParseFile);
+    myHashtagFile.open(hashtagFile);
+    myParseFile.open(parseFile);
+    while (!myHashtagFile.is_open()) {
+         cout << "This file does not exist.\n";
+        cout << "Welcome to the Hashtag Analyzer \nYou will choose a file of hashtags to search for \nThen you will choose a file to search for the hashtags" << endl;
+    cout << "Please enter the file with the hashtags you would like to use:" << endl; 
+	cin >> hashtagFile; //console input
+    cout << "Please enter the file with the messages you would like to parse:" << endl; 
+	cin >> parseFile; //console input
+
+    };
+      cout << "Your file was imported!\n" << endl;
+
     
+        while (getline(myHashtagFile, hashtag)){
+            hashtagSize++;
+            cout << hashtagSize << "\t"<< hashtag << endl;  
+        }
+        cout << hashtagSize << " hashtags loaded." << endl;
+    
+
+    string hashtagArray[hashtagSize];
+    int count[hashtagSize];
+ 
+     if (myParseFile.is_open()) {
+        cout << "Your file was imported!\n" << endl;
+    while (getline(myParseFile, hashTag)){
+        
+        
+        getline(myParseFile, parse);
+        
+        // for (int i = 0; i < 5; i++) {
+
+        // }
+        // char firstParse = parse[0];
+        // if (firstParse == '#'){
+            parseSize++;
+         
+        // }
+        cout << "Messages with matching hashtags\n";
+        cout << parseSize << " . " << hashTag  << " " << parse << endl;
+        //  myParseFile >> parse;
+//  myParseFile >> hashtag;
+//     myParseFile >> parse;
+//     cout << " Hashtag" << hashtag << endl;
+//     cout << " message" << parse << endl;
+    }
+   
+    cout << parseSize << " message loaded." << endl;
     }else {
-      cout << "Unable to open file"; //If the input file does not exist, notifies user
+      cout << "This file does not exist."; //If the input file does not exist, notifies user
     }
       myHashtagFile.close(); //Closes the file
   return 0;
 }
-
