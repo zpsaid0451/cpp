fb3c728e6642d442363e1e419887856ef2b8847c
Ping <zpsaid0451@gmail.com>
2022-10-10 17:27:46 -0400

    finish Track cpp

--- proj2/Track.cpp
+++ proj2/Track.cpp
@@ -8,10 +8,11 @@
 // Postconditions - Creates new Track with a default length 4 and each m_track = 0
 Track::Track() {
 	m_length = 4;
-	m_track[0] = 0;
-	m_track[1] = 0;
-	m_track[2] = 0;
-	m_track[3] = 0;
+	ResetLocation();
+	// m_track[0] = 0;
+	// m_track[1] = 0;
+	// m_track[2] = 0;
+	// m_track[3] = 0;
 
 }
 
@@ -35,9 +36,65 @@ void Track:: PopulateObstacles(){
 // Desc - Returns the status of race
 // Preconditions - Track exists
 // Postconditions - Outputs where each racer is in track and the type of obstacle
+void Track:: GetStatus(){
+
+	for (int player = 0; player < NUM_RACERS; player++){ 
+		string obstacleName = ConvertObstacle(player);
+		if (player = 0){
+			cout << "You are on a " << obstacleName << " in (obstacle " << m_track[player] << " of " << m_length <<" )"<< endl;
+		} else {
+			cout << "Player " << player+1 << "is on a " << obstacleName << " in (obstacle " << m_track[player] << " of " << m_length <<" )" << endl;
+		}
+        
+    }
+}
+
+// Name: CheckWin()
+// Desc - Checks to see if the race is over by comparing each racer's
+//        location to the length of the track.
+//        Returns number of player who wins race (always starts at 0 first)
+// Preconditions - Track exists
+// Postconditions - Returns integer location of winner (0 is human). Always returns human in case of tie!
+int Track:: CheckWin(){
+	for (int i = 0; i < NUM_RACERS; i++){
+		if (m_track[i] == m_length){
+			return 
+		}
+
+
+	
+}
+
+ // Name: GetObstacle
+  // Desc - Returns the integer of the obstacle at a specific location
+  // Preconditions - Track exists
+  // Postconditions - Returns 0 for speed, 1 for agility, and 2 for jump
 int Track::GetObstacle(int location){
 	return m_obstacle[location];
 
+}
+
+
+// Name: IncrementLocation
+// Desc - Increments the m_track value for a specific player the human player
+//        is always in position 0
+// Preconditions - Race starts with everyone having a 0
+// Postconditions - Increases the integer value for a specific player
+void Track:: IncrementLocation(int player){
+	if (m_track[player] < m_length){
+		m_track[player] ++;
+	} 
+}
+
+
+// Name: ResetLocation
+// Desc - Before each race, resets the starting location for all players back to 0
+// Preconditions - Track exists
+// Postconditions - All players has a starting location of 0 in the m_track
+void Track::ResetLocation(){
+	for (int i = 0; i < NUM_RACERS; i++){ 
+        m_track[i] = 0;
+    }
 }
 
 // Name: ConvertObstacle
@@ -47,35 +104,6 @@ int Track::GetObstacle(int location){
 // Postconditions - Pulls the location from m_track for a provided player
 //                  then returns the obstacle at that location
 string Track::ConvertObstacle(int player){
-
-	location = m_track[player]
+	int location = m_track[player]
     return OBSTACLE_TYPE[GetObstacle(location)];
-}
-
- // Name: GetStatus()
-  // Desc - Returns the status of race
-  // Preconditions - Track exists
-  // Postconditions - Outputs where each racer is in track and the type of obstacle
-void Track:: GetStatus(){
-	// string obstacleName = ConvertObstacle(int player);
-	// cout << "You are on a "<<s>> obstacle (obstacle" << m_track[0] << "of "<< m_length << ")" << endl;
-	// 				// get race status
-	// 				cout <<"Player 1 is on a speed obstacle in (obstacle 1 of 3)\n
-	// 				Player 2 is on a speed obstacle in (obstacle 1 of 3)\n
-	// 				Player 3 is on a speed obstacle in (obstacle 1 of 3)\n"
-
-}
-
-
-void Track:: IncrementLocation(int player){
-
-}
-
-
-int Track::CheckWin(){
-	return 1;
-}
-
-void Track::ResetLocation(){
-
 }
